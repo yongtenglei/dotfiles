0807f3a99095017a750b1ba11ea58e62de232ac6
yongtenglei <65058560+yongtenglei@users.noreply.github.com>
2021-08-30 09:20:31 +0800

    Update vi.zsh

--- zsh/vi.zsh
+++ zsh/vi.zsh
@@ -1,4 +1,4 @@
-#bindkey '^v' edit-command-line
+bindkey '^v' edit-command-line
 bindkey -v
 bindkey -M vicmd "k" vi-insert
 bindkey -M vicmd "K" vi-insert-bol
@@ -11,13 +11,13 @@ bindkey -M vicmd "u" up-line-or-history
 bindkey -M vicmd "l" undo
 #bindkey -M vicmd "-" vi-rev-repeat-search
 bindkey -M vicmd "=" vi-repeat-search
-bindkey -M vicmd "w" vi-forward-word-end
+bindkey -M vicmd "h" vi-forward-word-end
 
-function zle-keyma-select {
-	if [[ ${KEYMAP} == vicmd ]] || [[ $1 = 'block' ]]; then
-		echo -ne '\e[1 q'
-	elif [[ ${KEYMAP} == main ]] || [[ ${KEYMAP} == viins ]] || [[ ${KEYMAP} = '' ]] || [[ $1 = 'beam' ]]; then
-		echo -ne '\e[5 q'
+function zle-keymap-select {
+  if [[ ${KEYMAP} == vicmd ]] || [[ $1 = 'block' ]]; then
+    echo -ne '\e[1 q'
+  elif [[ ${KEYMAP} == main ]] || [[ ${KEYMAP} == viins ]] || [[ ${KEYMAP} = '' ]] || [[ $1 = 'beam' ]]; then
+    echo -ne '\e[5 q'
   fi
 }
 zle -N zle-keymap-select
@@ -27,12 +27,13 @@ echo -ne '\e[5 q'
 
 # Use beam shape cursor for each new prompt.
 preexec() {
-	echo -ne '\e[5 q'
+  echo -ne '\e[5 q'
 }
 
 _fix_cursor() {
-	echo -ne '\e[5 q'
+  echo -ne '\e[5 q'
 }
 precmd_functions+=(_fix_cursor)
 
 KEYTIMEOUT=1
+
