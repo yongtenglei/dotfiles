3e7ca4d7db407859b4d77fd36c9eb0caf62bb381
yongtenglei <65058560+yongtenglei@users.noreply.github.com>
2021-09-19 01:36:17 +0800

    Delete scripts directory

--- scripts/autostart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-picom -b
-fcitx5 &
-nm-applet &
-xset r rate 300 50
-/bin/bash ~/scripts/dwm/dwm-status.sh &
-/bin/bash ~/scripts/wp.sh &
-
--- scripts/redshift.sh
+++
@@ -1,5 +0,0 @@
-# !/bin/bash
-killall redshift
-sleep 1.5
-sudo redshift -l 31.22:121.48
-
