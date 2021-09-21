d7ac3208181cb33c0bcac8b49e90c576f6dd0db0
yongtenglei <65058560+yongtenglei@users.noreply.github.com>
2021-09-21 14:15:29 +0800

    Add files via upload

---
+++ zsh/env.zsh
@@ -0,0 +1,14 @@
+export EDITOR=/usr/bin/nvim
+export VISUAL=/usr/bin/nvim
+#export GOROOT=$HOME/go
+export GO111MODULE="on"
+export PATH=$PATH:/usr/local/go/bin
+export GOPROXY=https://goproxy.cn,direct
+
+
+# proxy
+export http_proxy=http://127.0.0.1:7890
+export https_proxy=http://127.0.0.1:7890
+
+#script
+export PATH=$PATH:~/scripts/somethingUseful
